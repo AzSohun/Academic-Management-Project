875a635ef3f5aea82c302e7a134dbebd4165ee16
AzSohun <asaduzzamansohun5@gmail.com>
2026-08-08 03:49:44 +0600

    Updated: Migration

---
+++ Migrations/20260807212143_FixedAllTheModels.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedAllTheModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TeacherId",
+                table: "Teachers",
+                newName: "TeacherCode");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleNo",
+                table: "Students",
+                newName: "RollNo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDraft",
+                table: "Assignments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDraft",
+                table: "Assignments");
+
+            migrationBuilder.RenameColumn(
+                name: "TeacherCode",
+                table: "Teachers",
+                newName: "TeacherId");
+
+            migrationBuilder.RenameColumn(
+                name: "RollNo",
+                table: "Students",
+                newName: "RoleNo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
